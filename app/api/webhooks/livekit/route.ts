338b54a76658546225779137d9b5289121422cca
nguyenvanquyet02 <quyet2k2ocsa@gmail.com>
2024-04-09 17:00:41 +0700

    push code

---
+++ app/api/webhooks/livekit/route.ts
@@ -0,0 +1,44 @@
+import { headers } from "next/headers";
+import { WebhookReceiver } from "livekit-server-sdk";
+import { LiveKitEvents } from "../../constants";
+
+import { db } from "@/lib/db";
+
+const receiver = new WebhookReceiver(
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const headerPayload = headers();
+  const authorization = headerPayload.get("Authorization");
+
+  if (!authorization) {
+    return new Response("No authorization header", { status: 400 });
+  }
+
+  const event = receiver.receive(body, authorization);
+
+  if (event.event === LiveKitEvents.IngressStarted) {
+    await db.stream.update({
+      where: {
+        ingressId: event.ingressInfo?.ingressId,
+      },
+      data: {
+        isLive: true,
+      },
+    });
+  }
+
+  if (event.event === LiveKitEvents.IngressEnded) {
+    await db.stream.update({
+      where: {
+        ingressId: event.ingressInfo?.ingressId,
+      },
+      data: {
+        isLive: false,
+      },
+    });
+  }
+}
